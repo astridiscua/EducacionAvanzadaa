6dc3f263bafb2212d683920d8c39e88e4a5f3a9d
astridiscua <aadiscuav@gmail.com>
2019-03-10 22:34:01 -0600

    agregando clases

---
+++ educacionRespaldo-master/EducacionAvanzada/EducacionAvanzada.BL/Nota.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducacionAvanzada.BL
+{
+    public class Nota
+    {
+        public int Id { get; set; }
+
+        public int IdGrado { get; set; }
+        public Grado Grado { get; set; }
+
+        public int IdJornada { get; set; }
+        public Jornada Jornada { get; set; }
+
+        public int IdSeccion { get; set; }
+        public seccion Seccion { get; set; }
+
+        public List<NotasDetalle> ListadeNotasDetalle { get; set; }
+
+        public Nota()
+        {
+            ListadeNotasDetalle = new List<NotasDetalle>();
+        }
+    }
+
+    public class NotasDetalle
+    {
+        public int Id { get; set; }
+
+        public int NotaId { get; set; }
+        public Nota Nota { get; set; }
+
+        public int AlumnoId { get; set; }
+        public Alumno Alumno { get; set; }
+
+        public int MateriaId { get; set; }
+        public materia Materia { get; set; }
+
+        public int PrimerParcial { get; set; }
+        public int SegundoParcial { get; set; }
+        public int TercerParcial { get; set; }
+        public int CuartoParcial { get; set; }
+
+        public int NotaFInal { get; set; }
+    }
+}
---
+++ educacionRespaldo-master/EducacionAvanzada/EducacionAvanzada.BL/NotaBL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducacionAvanzada.BL
+{
+    public class NotaBL
+    {
+        Contexto _contexto;
+        public List<Nota> ListadeNota { get; set; }
+
+        public NotaBL()
+        {
+            _contexto = new Contexto();
+            ListadeNota = new List<Nota>();
+        }
+        public List<Nota> ObtenerNota()
+        {
+            ListadeNota = _contexto.Notas
+            .Include("Alumno")
+            .ToList();
+
+            return ListadeNota;
+        }
+}
