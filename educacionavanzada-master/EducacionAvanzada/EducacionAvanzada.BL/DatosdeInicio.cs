9b6a22545f193c69a205f4af0f915430846433b3
astridiscua <aadiscuav@gmail.com>
2019-04-22 22:00:16 -0600

    agregando usuario

---
+++ educacionavanzada-master/EducacionAvanzada/EducacionAvanzada.BL/DatosdeInicio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducacionAvanzada.BL
+{
+    
+        public class DatosdeInicio : CreateDatabaseIfNotExists<Contexto>
+        {
+            protected override void Seed(Contexto contexto)
+            {
+                var nuevoUsuario = new Usuario();
+                nuevoUsuario.Nombre = "admin";
+                nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
+
+            var nuevoUsuario1 = new Usuario();
+            nuevoUsuario.Nombre = "astrid";
+            nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
+
+            var nuevoUsuario2 = new Usuario();
+            nuevoUsuario.Nombre = "jaime";
+            nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
+
+            var nuevoUsuario3 = new Usuario();
+            nuevoUsuario.Nombre = "carolina";
+            nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
+
+            var nuevoUsuario4 = new Usuario();
+            nuevoUsuario.Nombre = "anner";
+            nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
+            contexto.Usuarios.Add(nuevoUsuario);
+
+                base.Seed(contexto);
+            }
+}
+}
+
---
+++ educacionavanzada-master/EducacionAvanzada/EducacionAvanzada.Web/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using EducacionAvanzada.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace EducacionAvanzada.Web.Controllers
+{
+    [Authorize]
+    public class LoginController : Controller
+    {
+        SeguridaBL _seguridadBL;
+        AlumnosBL _alumnosBL;
+        PadresBL _padresBL;
+        public LoginController()
+        {
+            _seguridadBL = new SeguridaBL();
+            _alumnosBL = new AlumnosBL();
+            _padresBL = new PadresBL();
+        }
+
+     
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            FormsAuthentication.SignOut();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection data)
+        {
+           
+            var nombreUsuario = data["usuario"];
+            var contrasena = data["Contrasena"];
+
+            var usuarioValido = _seguridadBL
+                .Autorizar(nombreUsuario, contrasena);
+            
+           
+            if (usuarioValido)
+            {
+                FormsAuthentication.SetAuthCookie(nombreUsuario, true);
+                
+                return RedirectToAction("Index", "Alumnos", new { });
+                
+            }
+
+            ModelState.AddModelError("", "Usuario o contraseña invalido");
+
+            return View();
+        }
+    }
+}
